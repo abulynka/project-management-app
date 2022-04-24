8fc4ce3d816cc02beb0adefbc52072cc18d8c63b
Aliaksandr Bulynka <abulynka@gmail.com>
2022-04-25 01:20:38 +0200

    fix: correct files according to the eslint and prettier rules

--- src/test.ts
+++ src/test.ts
@@ -4,11 +4,15 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     <T>(id: string): T;
     keys(): string[];
   };
@@ -21,6 +25,10 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context: { <T>(id: string): T; keys(): string[] } = require.context(
+  './',
+  true,
+  /\.spec\.ts$/,
+);
 // And load the modules.
 context.keys().map(context);
