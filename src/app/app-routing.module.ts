6e356b62b4f6fa57ebc01cff41f7c55ededf66a2
Aliaksandr Bulynka <abulynka@gmail.com>
2022-04-27 17:28:12 +0200

    refactor: set modules as lazy modules instead of routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,18 +11,24 @@ const routes: Routes = [
     path: 'boards',
     loadChildren: async () =>
       import('./project-management/project-management.module').then(
-        (m) => m.ProjectManagementModule,
+        // eslint-disable-next-line @typescript-eslint/typedef
+        (m) => {
+          return m.ProjectManagementModule;
+          // eslint-disable-next-line @typescript-eslint/comma-dangle
+        }
       ),
   },
   {
     path: 'login',
     loadChildren: async () =>
-      import('./auth/auth-routing.module').then((m) => m.AuthRoutingModule),
+      // eslint-disable-next-line @typescript-eslint/typedef
+      import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: '**',
     loadChildren: async () =>
-      import('./core/core-routing.module').then((m) => m.CoreRoutingModule),
+      // eslint-disable-next-line @typescript-eslint/typedef
+      import('./core/core.module').then((m) => m.CoreModule),
   },
 ];
 
