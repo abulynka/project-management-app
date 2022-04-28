7b322f92a88b9fe19da7b98ad1732dc8bfa1f80a
Aliaksandr Bulynka <abulynka@gmail.com>
2022-04-28 22:45:28 +0200

    feat: add not found board component

--- src/app/project-management/project-management.module.ts
+++ src/app/project-management/project-management.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectManagementRoutingModule } from './project-management-routing.module';
 import { BoardsComponent } from './pages/boards/boards.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [BoardsComponent],
+  declarations: [BoardsComponent, NotFoundComponent],
   imports: [CommonModule, ProjectManagementRoutingModule],
   exports: [BoardsComponent],
 })
