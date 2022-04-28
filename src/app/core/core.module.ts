b5e1920541003efbff28305c1704108072181612
Aliaksandr Bulynka <abulynka@gmail.com>
2022-04-28 23:56:58 +0200

    feat: add necessary modules includes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,21 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
-  imports: [CommonModule, CoreRoutingModule],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    TranslateModule,
+  ],
   exports: [HeaderComponent, FooterComponent, NotFoundComponent],
 })
 export class CoreModule {}
