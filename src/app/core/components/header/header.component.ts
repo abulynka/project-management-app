a3acf7852e2a7b0fe75c35a44288c54a63e292a4
Barodzich Aliaksandr <aliaksandr.barodzich@innowise-group.com>
2022-04-30 21:51:33 +0300

    refactor: apply lang-service and langTitleMap

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,42 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { TranslateService } from '@ngx-translate/core';
+import { ILang, LangService } from '../../services/lang.service';
+import { ILangTitleItem, langTitleMap } from './utils/languageTitleMap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  @Input() public languageTitle: 'En' | 'Ru' = 'En';
-
-  @Input() public languageTitleLarge: 'English' | 'Russian' = 'English';
+export class HeaderComponent implements OnInit {
+  public langTitle!: ILangTitleItem;
 
   public isShowMenu: boolean = false;
 
-  public constructor(public translate: TranslateService) {}
+  public constructor(private langService: LangService) {}
 
   @Input() public changeLanguage = (event: MatSlideToggleChange): void => {
     if (event.checked) {
-      this.setLangage('ru');
+      this.setLanguageTitle('ru');
+      this.langService.setLang('ru');
     } else {
-      this.setLangage('en');
+      this.setLanguageTitle('en');
+      this.langService.setLang('en');
     }
   };
+
+  public ngOnInit(): void {
+    const curLang: ILang = this.langService.getLang();
+    this.setLanguageTitle(curLang);
+  }
 
   public toggle(): void {
     this.isShowMenu = !this.isShowMenu;
   }
 
-  private setLangage(language: 'en' | 'ru'): void {
-    if (language === 'en') {
-      this.languageTitle = 'En';
-      this.languageTitleLarge = 'English';
-      this.translate.use('en');
-    } else {
-      this.languageTitle = 'Ru';
-      this.languageTitleLarge = 'Russian';
-      this.translate.use('ru');
-    }
+  private setLanguageTitle(lang: ILang): void {
+    this.langTitle = langTitleMap[lang];
   }
 }
