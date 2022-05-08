22d5c22feaa513c18c1996673b41dd8e5994fd38
Alex Borodich <aliaksandr.barodzich@innowise-group.com>
2022-05-08 09:55:24 +0300

    chore: change mock data

--- src/app/project-management/pages/board/board.component.ts
+++ src/app/project-management/pages/board/board.component.ts
@@ -14,8 +14,36 @@ const board = {
       tasks: [
         {
           id: '6e3abe9c-ceb1-40fa-9a04-eb2b2184daf9',
-          title: 'Task: pet the cat',
+          title: 'Task: pet',
           order: 1,
+          done: false,
+          description: 'Domestic cat needs to be stroked gently',
+          userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
+          files: [
+            {
+              filename: 'foto.jpg',
+              fileSize: 6105000,
+            },
+          ],
+        },
+        {
+          id: '6e3abe9c-ceb1-40fa-9a04-eb2b2184daf9',
+          title: 'Task: the',
+          order: 2,
+          done: false,
+          description: 'Domestic cat needs to be stroked gently',
+          userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
+          files: [
+            {
+              filename: 'foto.jpg',
+              fileSize: 6105000,
+            },
+          ],
+        },
+        {
+          id: '6e3abe9c-ceb1-40fa-9a04-eb2b2184daf9',
+          title: 'Task: cat',
+          order: 3,
           done: false,
           description: 'Domestic cat needs to be stroked gently',
           userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
