5bd8779d35c9fd1e81f451901e1a048954df7a74
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-09 16:54:34 +0300

    feat: add edit board component

---
+++ src/app/project-management/components/edit-board/edit-board.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-board',
+  templateUrl: './edit-board.component.html',
+  styleUrls: ['./edit-board.component.scss'],
+})
+export class EditBoardComponent {}
