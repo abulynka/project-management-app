096cdd6054fae1af388e82ddef182be469ce2cae
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-10 19:51:55 +0300

    feat: add edit board functionality

--- src/app/project-management/pages/boards/boards.component.ts
+++ src/app/project-management/pages/boards/boards.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BoardsService } from '../../services/boards.service';
 import { BoardResponse, BoardShort } from '../../models/boards.model';
 import { TranslateService } from '@ngx-translate/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationModalComponent } from '../../../shared/confirmation-modal/confirmation-modal.component';
+import { EditBoardComponent } from '../../components/edit-board/edit-board.component';
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
   styleUrls: ['./boards.component.scss'],
 })
-export class BoardsComponent {
+export class BoardsComponent implements OnInit {
   public selectedOptions: string[] = [];
 
   public boards: BoardShort[] = [];
@@ -28,6 +29,12 @@ export class BoardsComponent {
 
   private static capitalizeFirstLetter(message: string): string {
     return message.charAt(0).toUpperCase() + message.slice(1);
+  }
+
+  public ngOnInit(): void {
+    this.boardsService.boardProcessed$.subscribe(() => {
+      this.refreshBoardsList();
+    });
   }
 
   public openBoard(): void {
@@ -66,9 +73,33 @@ export class BoardsComponent {
       });
   }
 
+  public editBoard(event: MouseEvent, id: string): void {
+    event.stopPropagation();
+
+    this.dialog
+      .open(EditBoardComponent, {
+        data: {
+          board: this.getBoard(id),
+        },
+      })
+      .componentInstance.boardProcessed$.subscribe(() => {
+        this.dialog.closeAll();
+      });
+  }
+
   private refreshBoardsList(): void {
     this.boardsService.getBoards().subscribe((response: BoardResponse[]) => {
-      this.boards = response;
+      this.boards = response.sort(
+        (item1: BoardResponse, item2: BoardResponse) => {
+          if (item1.title < item2.title) {
+            return -1;
+          } else if (item1.title === item2.title) {
+            return 0;
+          } else {
+            return 1;
+          }
+        },
+      );
     });
   }
 
@@ -76,5 +107,11 @@ export class BoardsComponent {
     return this.boards.find((board: BoardShort) => {
       return board.id === id;
     })?.title;
+  }
+
+  private getBoard(id: string): BoardShort | undefined {
+    return this.boards.find((board: BoardShort) => {
+      return board.id === id;
+    });
   }
 }
