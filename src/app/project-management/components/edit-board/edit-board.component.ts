e05fa031be1899e9eb849708cec12b7a68752a8e
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-10 19:49:11 +0300

    feat: add edit board component

--- src/app/project-management/components/edit-board/edit-board.component.ts
+++ src/app/project-management/components/edit-board/edit-board.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { BoardsService } from '../../services/boards.service';
+import { Board, BoardResponse, BoardShort } from '../../models/boards.model';
+import { Observable, Subject } from 'rxjs';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-board',
   templateUrl: './edit-board.component.html',
   styleUrls: ['./edit-board.component.scss'],
 })
-export class EditBoardComponent {}
+export class EditBoardComponent {
+  public boardForm: FormGroup = new FormGroup({
+    title: new FormControl(),
+  });
+
+  public board: Board | BoardShort | undefined;
+
+  public boardProcessedSource: Subject<string> = new Subject<string>();
+
+  public boardProcessed$: Observable<string> =
+    this.boardProcessedSource.asObservable();
+
+  public constructor(
+    private boardsService: BoardsService,
+    @Inject(MAT_DIALOG_DATA)
+    public data: { board: BoardShort | Board | undefined },
+  ) {
+    if (data) {
+      this.board = data.board;
+      this.boardForm.get('title')?.setValue(this.board?.title);
+    }
+  }
+
+  public onSubmit(): void {
+    let afterProcess: Observable<BoardResponse>;
+    if (this.board) {
+      afterProcess = this.boardsService.updateBoard(
+        this.board.id,
+        this.boardForm.get('title')?.value,
+      );
+    } else {
+      afterProcess = this.boardsService.createBoard(
+        this.boardForm.get('title')?.value,
+      );
+    }
+
+    afterProcess.subscribe((response: BoardResponse) => {
+      this.boardProcessedSource.next(response.id);
+    });
+  }
+}
