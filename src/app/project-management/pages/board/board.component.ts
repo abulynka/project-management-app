ddef17fc70f8b480f86840e435fbe22d7fa781f8
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-12 21:04:20 +0300

    feat: move board and boards components to redux

--- src/app/project-management/pages/board/board.component.ts
+++ src/app/project-management/pages/board/board.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 import { ColumnService } from '../../services/column.service';
 import { Board, Column } from '../../models/boards.model';
-import { BoardsService } from '../../services/boards.service';
+import { select, Store } from '@ngrx/store';
+import { ProjectManagementState } from '../../../redux/state.models';
+import { getBoardById } from '../../../redux/selectors/project-management.selector';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, OnDestroy {
   public board: Board = {} as Board;
 
   private columnList$: BehaviorSubject<[] | Column[]> | null = null;
+
+  private destroy$: Subject<void> = new Subject<void>();
 
   public constructor(
     private columnService: ColumnService,
     private route: ActivatedRoute,
     private router: Router,
-    private boardsService: BoardsService,
+    private store: Store<ProjectManagementState>,
   ) {}
 
   public ngOnInit(): void {
-    this.boardsService
-      .getBoardById(this.route.snapshot.params['id'])
-      .subscribe((board: Board): void => {
-        this.board = board;
-        this.initColumnListStateObserver();
+    this.store
+      .pipe(
+        select(getBoardById(this.route.snapshot.params['id'])),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((board: Board | undefined) => {
+        if (board) {
+          this.board = board;
+          this.initColumnListStateObserver();
+        }
       });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   public onAddColumn(title: string = 'default'): void {
