11152a6775693fe27719686d35c589d95540e7c6
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-12 21:10:46 +0300

    feat: add redux to edit board component

--- src/app/project-management/components/edit-board/edit-board.component.ts
+++ src/app/project-management/components/edit-board/edit-board.component.ts
@@ -1,16 +1,21 @@
-import { Component, Inject } from '@angular/core';
-import { BoardsService } from '../../services/boards.service';
-import { Board, BoardResponse, BoardShort } from '../../models/boards.model';
-import { Observable, Subject } from 'rxjs';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { Board, BoardShort } from '../../models/boards.model';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormControl, FormGroup } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { ProjectManagementState } from '../../../redux/state.models';
+import {
+  ProjectManagementActionType,
+  updatedBoard,
+} from '../../../redux/actions/project-management.action';
 
 @Component({
   selector: 'app-edit-board',
   templateUrl: './edit-board.component.html',
   styleUrls: ['./edit-board.component.scss'],
 })
-export class EditBoardComponent {
+export class EditBoardComponent implements OnDestroy {
   public boardForm: FormGroup = new FormGroup({
     title: new FormControl(),
   });
@@ -22,8 +27,10 @@ export class EditBoardComponent {
   public boardProcessed$: Observable<string> =
     this.boardProcessedSource.asObservable();
 
+  private destroy$: Subject<void> = new Subject<void>();
+
   public constructor(
-    private boardsService: BoardsService,
+    private store: Store<ProjectManagementState>,
     @Inject(MAT_DIALOG_DATA)
     public data: { board: BoardShort | Board | undefined },
   ) {
@@ -34,20 +41,29 @@ export class EditBoardComponent {
   }
 
   public onSubmit(): void {
-    let afterProcess: Observable<BoardResponse>;
     if (this.board) {
-      afterProcess = this.boardsService.updateBoard(
-        this.board.id,
-        this.boardForm.get('title')?.value,
-      );
+      this.store.dispatch({
+        type: ProjectManagementActionType.UpdateBoard,
+        payload: {
+          id: this.board.id,
+          title: this.boardForm.get('title')?.value,
+        },
+      });
+      this.store
+        .pipe(select(updatedBoard), takeUntil(this.destroy$))
+        .subscribe(() => {
+          this.boardProcessedSource.next(`${this.board?.id}`);
+        });
     } else {
-      afterProcess = this.boardsService.createBoard(
-        this.boardForm.get('title')?.value,
-      );
+      this.store.dispatch({
+        type: ProjectManagementActionType.CreateBoard,
+        payload: this.boardForm.get('title')?.value,
+      });
     }
+  }
 
-    afterProcess.subscribe((response: BoardResponse) => {
-      this.boardProcessedSource.next(response.id);
-    });
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
