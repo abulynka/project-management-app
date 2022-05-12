275b348042f1330f4fd734c7a86879278ff8a600
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-12 21:01:22 +0300

    feat: move board service to redux

--- src/app/project-management/services/boards.service.ts
+++ src/app/project-management/services/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Board, BoardResponse } from '../models/boards.model';
 import { apiRoot } from 'src/environments/environment';
 
@@ -8,11 +8,6 @@ import { apiRoot } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class BoardsService {
-  public boardProcessedSource: Subject<void> = new Subject<void>();
-
-  public boardProcessed$: Observable<void> =
-    this.boardProcessedSource.asObservable();
-
   public constructor(public http: HttpClient) {}
 
   public getBoards(): Observable<BoardResponse[]> {
@@ -20,38 +15,18 @@ export class BoardsService {
   }
 
   public createBoard(title: string): Observable<BoardResponse> {
-    // todo: remove this pipe after ngrx will be implemented
-    return this.http.post<BoardResponse>(`${apiRoot}/boards`, { title }).pipe(
-      map((data: BoardResponse): BoardResponse => {
-        this.boardProcessedSource.next();
-        return data;
-      }),
-    );
+    return this.http.post<BoardResponse>(`${apiRoot}/boards`, { title });
   }
 
   public getBoardById(id: string): Observable<Board> {
     return this.http.get<Board>(`${apiRoot}/boards/${id}`);
   }
 
-  public deleteBoard(id: string): Observable<BoardResponse> {
-    // todo: remove this pipe after ngrx will be implemented
-    return this.http.delete<BoardResponse>(`${apiRoot}/boards/${id}`).pipe(
-      map((data: BoardResponse): BoardResponse => {
-        this.boardProcessedSource.next();
-        return data;
-      }),
-    );
+  public deleteBoard(id: string): Observable<void> {
+    return this.http.delete<void>(`${apiRoot}/boards/${id}`);
   }
 
   public updateBoard(id: string, title: string): Observable<BoardResponse> {
-    // todo: remove this pipe after ngrx will be implemented
-    return this.http
-      .put<BoardResponse>(`${apiRoot}/boards/${id}`, { title })
-      .pipe(
-        map((data: BoardResponse): BoardResponse => {
-          this.boardProcessedSource.next();
-          return data;
-        }),
-      );
+    return this.http.put<BoardResponse>(`${apiRoot}/boards/${id}`, { title });
   }
 }
