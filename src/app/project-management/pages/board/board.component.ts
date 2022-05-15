76a9cf38d6a5c19c0077384a77c0ce2534dd1d49
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-15 19:42:06 +0300

    feat: improve board component

--- src/app/project-management/pages/board/board.component.ts
+++ src/app/project-management/pages/board/board.component.ts
@@ -5,12 +5,16 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 import { ColumnService } from '../../services/column.service';
-import { Board, Column, Task } from '../../models/boards.model';
+import { BoardShort, Column, Task } from '../../models/boards.model';
 import { select, Store } from '@ngrx/store';
-import { ProjectManagementState } from '../../../redux/state.models';
-import { getBoardById } from '../../../redux/selectors/project-management.selector';
+import { BoardsState } from '../../../redux/state.models';
+import { getBoardById } from '../../../redux/selectors/boards.selector';
 
 import { TaskService } from '../../services/task/task.service';
+import { ColumnsService } from '../../services/columns.service';
+import { BoardsService } from '../../services/boards.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { EditColumnComponent } from '../../components/edit-column/edit-column.component';
 
 @Component({
   selector: 'app-board',
@@ -18,7 +22,9 @@ import { TaskService } from '../../services/task/task.service';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit, OnDestroy {
-  public board: Board = {} as Board;
+  public board: BoardShort = {} as BoardShort;
+
+  public columns: Column[] = [];
 
   public title: string = '';
 
@@ -31,8 +37,11 @@ export class BoardComponent implements OnInit, OnDestroy {
   public constructor(
     private columnService: ColumnService,
     private route: ActivatedRoute,
-    private store: Store<ProjectManagementState>,
+    private store: Store<BoardsState>,
     private taskService: TaskService,
+    private columnsService: ColumnsService,
+    private boardsService: BoardsService,
+    private dialog: MatDialog,
   ) {}
 
   public ngOnInit(): void {
@@ -41,11 +50,17 @@ export class BoardComponent implements OnInit, OnDestroy {
         select(getBoardById(this.route.snapshot.params['id'])),
         takeUntil(this.destroy$),
       )
-      .subscribe((board: Board | undefined) => {
+      .subscribe((board: BoardShort | undefined) => {
         if (board) {
-          this.board = board;
-          this.title = this.board.title;
-          this.initColumnListStateObserver();
+          this.columnsService
+            .getAllColumns(board.id)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((columns: any) => {
+              this.columns = columns;
+              this.board = board;
+              this.title = this.board.title;
+              this.initColumnListStateObserver();
+            });
         }
       });
   }
@@ -55,8 +70,16 @@ export class BoardComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  public onAddColumn(title: string = 'default'): void {
-    this.columnService.add(title);
+  public onAddColumn(): void {
+    const dialogInstance: MatDialogRef<EditColumnComponent> =
+      this.dialog.open(EditColumnComponent);
+
+    dialogInstance.componentInstance.columnProcessed$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.columnService.add(dialogInstance.componentInstance.title);
+        dialogInstance.close();
+      });
   }
 
   public dropColumn(event: CdkDragDrop<any[]>): void {
@@ -67,7 +90,7 @@ export class BoardComponent implements OnInit, OnDestroy {
         event.currentIndex,
       );
       this.columnService.updateOrder(
-        this.board.columns,
+        this.columns,
         event.previousIndex,
         event.currentIndex,
       );
@@ -88,14 +111,19 @@ export class BoardComponent implements OnInit, OnDestroy {
 
   public titleSwitchClickAndSave(): void {
     this.titleSwitchClick();
+    this.boardsService.updateBoard(
+      this.board.id,
+      this.title,
+      this.board.description,
+    );
   }
 
   private initColumnListStateObserver(): void {
-    this.columnList$ = this.columnService.setState(this.board.columns);
+    this.columnList$ = this.columnService.setState(this.columns);
     this.columnList$
       .pipe(takeUntil(this.destroy$))
       .subscribe((columns: Column[]) => {
-        this.board.columns = columns;
+        this.columns = columns;
       });
   }
 }
