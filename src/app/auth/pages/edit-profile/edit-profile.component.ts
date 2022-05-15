89fe748440f7966cec2d3f7f9485905ca15a91d2
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-15 10:02:27 +0300

    fix: solve edit profile problem

--- src/app/auth/pages/edit-profile/edit-profile.component.ts
+++ src/app/auth/pages/edit-profile/edit-profile.component.ts
@@ -7,7 +7,9 @@ import {
 } from '@angular/forms';
 import { TokenStorageService } from 'src/app/core/services/token-storage.service';
 import { UserService } from 'src/app/project-management/services/user.service';
-import { SignUpData } from '../../models/authorization.model';
+import { SignUpData, SignUpResponse } from '../../models/authorization.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-edit-profile',
@@ -17,12 +19,14 @@ import { SignUpData } from '../../models/authorization.model';
 export class EditProfileComponent implements OnInit {
   public updateForm: FormGroup = {} as FormGroup;
 
-  public userId: string | undefined = this.storageService.getUser()?.id;
+  public user: SignUpResponse | null = this.storageService.getUser();
 
   public constructor(
     public userService: UserService,
     public storageService: TokenStorageService,
     private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private translate: TranslateService,
   ) {}
 
   public ngOnInit(): void {
@@ -30,18 +34,30 @@ export class EditProfileComponent implements OnInit {
   }
 
   public onSubmit(value: SignUpData): void {
-    if (this.updateForm.status === 'VALID') {
-      this.userService.updateUser(this.userId as string, value).subscribe({
-        next: () => {},
+    if (this.updateForm.status === 'VALID' && this.user) {
+      this.userService.updateUser(this.user.id, value).subscribe({
+        next: () => {
+          this.translate
+            .get('auth.profile-saved')
+            .subscribe((message: string) => {
+              this.snackBar.open(message, '', {
+                duration: 3000,
+                panelClass: ['green-snackbar'],
+              });
+            });
+        },
       });
     }
   }
 
   public initUpdateForm(): void {
     this.updateForm = this.formBuilder.group({
-      login: new FormControl(null, [Validators.required, Validators.email]),
-      name: new FormControl(null, [Validators.required]),
-      password: new FormControl(null, [Validators.required]),
+      login: new FormControl(this.user?.login, [
+        Validators.required,
+        Validators.email,
+      ]),
+      name: new FormControl(this.user?.name, [Validators.required]),
+      password: new FormControl('', [Validators.required]),
     });
   }
 }
