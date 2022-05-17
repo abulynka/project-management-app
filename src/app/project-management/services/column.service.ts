d9c3e9d07db0d5083fd14c4e1071fd9508ddfa8e
Aliaksandr Bulynka <abulynka@gmail.com>
2022-05-17 22:08:12 +0300

    feat: change column service

--- src/app/project-management/services/column.service.ts
+++ src/app/project-management/services/column.service.ts
@@ -1,63 +1,87 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Column, ColumnResponse, Task } from '../models/boards.model';
+import { Column, ColumnResponse, NewTask, Task } from '../models/boards.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ColumnService {
-  private columnList: ColumnResponse[] | [] = [];
+  private columnList: ColumnResponse[] = [];
 
-  private columnList$: BehaviorSubject<[] | ColumnResponse[]> =
-    new BehaviorSubject(this.columnList);
+  private tasks: Task[] = [];
 
-  public getState(): BehaviorSubject<[] | ColumnResponse[]> {
+  private columnList$: BehaviorSubject<ColumnResponse[]> = new BehaviorSubject(
+    this.columnList,
+  );
+
+  public getState(): BehaviorSubject<ColumnResponse[]> {
     return this.columnList$;
   }
 
   public setState(
     columns: ColumnResponse[],
-  ): BehaviorSubject<[] | ColumnResponse[]> {
+  ): BehaviorSubject<ColumnResponse[]> {
     this.columnList = columns;
     this.columnList$.next(this.columnList);
     return this.columnList$;
   }
 
-  public add(title: string): ColumnResponse {
-    const column: ColumnResponse = this.newColumn(title);
+  public add(column: ColumnResponse): void {
     this.columnList$.next(this.pushNewItem(column));
-    return column;
   }
 
   public updateOrder(
     list: ColumnResponse[],
     prevIdx: number,
     curIdx: number,
-  ): void {
+  ): ColumnResponse[] {
     list[curIdx].order = curIdx + 1;
     list[prevIdx].order = prevIdx + 1;
 
+    const updateOrders: ColumnResponse[] = [list[curIdx], list[prevIdx]];
+
+    let index: number = Math.min(curIdx, prevIdx);
+    for (let i: number = index; i < list.length; ++i) {
+      if (list[i].order !== i + 1) {
+        list[i].order = i + 1;
+        updateOrders.push(list[i]);
+      }
+    }
+
     this.columnList = list;
     this.columnList$.next(list);
+
+    return updateOrders;
+  }
+
+  public newTask(newTask: NewTask): NewTask {
+    return {
+      ...newTask,
+      order: Math.max(...this.tasks.map((task: NewTask) => task.order), 0) + 1,
+    };
   }
 
   public addTask(task: Task): void {
+    this.tasks.push(task);
+
     // const columnIndex: number = this.columnList.findIndex(
     //   (column: ColumnResponse) => column.id === task.columnId,
     // );
     // const selectedColumn: ColumnResponse = this.columnList[columnIndex];
     // selectedColumn.tasks = [...selectedColumn.tasks, task];
 
-    console.log(task);
-
     this.columnList$.next(this.columnList);
   }
 
-  private newColumn(title: string): Column {
+  public newColumn(title: string): Column {
     return {
-      id: String(this.columnList.length + 1),
+      id: '',
       title,
-      order: this.columnList.length + 1,
+      order:
+        Math.max(
+          ...this.columnList.map((column: ColumnResponse) => column.order),
+          0,
+        ) + 1,
       tasks: [],
     };
   }
